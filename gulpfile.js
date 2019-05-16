35d13e55b4ca0ba8ef370f95925f1296342c75a9
AlinaNiko <alinanikolaenko91@gmail.com>
2019-05-16 17:41:31 +0300

    Изменена структура проекта, svg-иконки блока простая аутентификация, перенесены из разметки в стили

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,30 @@
-"use strict";
+'use strict';
 
-const gulp = require('gulp'),
-    posthtml = require('gulp-posthtml'),
-    include = require('posthtml-include'),
-    scss = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    csscomb = require('gulp-csscomb'),
-    csso = require('gulp-csso'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    imageminMozjpeg = require('imagemin-mozjpeg'),
-    webp = require('imagemin-webp'),
-    svgstore = require('gulp-svgstore'),
-    svgMinify = require('gulp-svgmin'),
-    ttf2woff = require('gulp-ttf2woff'),
-    ttf2woff2 = require('gulp-ttf2woff2'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    browserSync = require('browser-sync').create();
+const gulp = require( 'gulp' );
+const posthtml = require( 'gulp-posthtml' );
+const include = require( 'posthtml-include' );
+const scss = require( 'gulp-sass' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const autoprefixer = require( 'gulp-autoprefixer' );
+const csscomb = require( 'gulp-csscomb' );
+const csso = require( 'gulp-csso' );
+const criticalCss = require( 'gulp-critical-css' );
+const imagemin = require( 'gulp-imagemin' );
+const imageminMozjpeg = require( 'imagemin-mozjpeg' );
+const webp = require( 'imagemin-webp' );
+const svgSprite = require( 'gulp-svg-sprite' );
+const svgMinify = require( 'gulp-svgmin' );
+const ttf2woff = require( 'gulp-ttf2woff' );
+const ttf2woff2 = require( 'gulp-ttf2woff2' );
+const plumber = require( 'gulp-plumber' );
+const debug = require( 'gulp-debug' );
+const notify = require( 'gulp-notify' );
+const rename = require( 'gulp-rename' );
+const newer = require( 'gulp-newer' );
+const uglify = require( 'gulp-uglify' );
+const gulpIf = require( 'gulp-if' );
+const del = require( 'del' );
+const browserSync = require( 'browser-sync' ).create();
 
 const root = {
   src: 'src/',
@@ -34,7 +40,8 @@ const path = {
     img: root.src + 'images/',
     font: root.src + 'fonts/**/*.ttf',
     ico: root.src + 'icons/**/*.svg',
-    vid: root.src + 'video/**/*.mp4'
+    vid: root.src + 'video/**/*.mp4',
+    ven: root.src + 'vendor/**/*.js'
   },
   build: {
     php: root.build,
@@ -44,7 +51,8 @@ const path = {
     img: root.build + 'images/',
     font: root.build + 'fonts/',
     ico: root.build + 'icons/',
-    vid: root.build + 'videos/'
+    vid: root.build + 'videos/',
+    ven: root.build + 'vendor/'
   },
   watch: {
     php: root.src + '*.php',
@@ -61,20 +69,18 @@ const path = {
     img: root.src + 'images/',
     font: root.src + 'fonts/**/*.ttf',
     ico: root.src + 'icons/**/*.svg',
-    vid: root.src + 'video/**/*.mp4'
+    vid: root.src + 'video/**/*.mp4',
+    ven: root.src + 'vendor/**/*.js'
   }
 };
 
 
-// Clean dir ============================
+// CLEAN ============================
 
-function clean() {
-  console.log('Очистка директории...');
-  return del([
-    'build/**',
-    '!build',
-    '!build/vendor/**'
-  ]);
+function cleanDirectory() {
+
+  return del(['build']);
+
 }
 
 
@@ -82,59 +88,95 @@ function clean() {
 
 function compilePHP() {
 
-  console.log('Переброс PHP...');
+  return gulp.src( path.src.php )
+    .pipe( gulp.dest( path.build.php ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.php)
-    .pipe(gulp.dest(path.build.php))
-    .pipe(browserSync.stream());
 }
 
 
 // HTML ============================
 
-function compileMarkup() {
+function compileHTML() {
 
-  console.log('Компиляция HTML...');
+  return gulp.src( path.src.html )
+    .pipe( plumber( {
+      errorHandler: notify.onError( {
+        title: "Ошибка в HTML",
+        message: "Error: <%= error.message %>"
+      } )
+    } ) )
+    .pipe( posthtml([
+      include()
+    ]) )
+    .pipe( gulp.dest( path.build.html ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.html)
-    .pipe(posthtml([ include() ]))
-    .pipe(gulp.dest(path.build.html))
-    .pipe(browserSync.stream());
 }
 
 
 // CSS ============================
 
-function compileStyles() {
+function compileCSS() {
 
-  console.log('Компиляция CSS...');
+  return gulp.src( path.src.css )
+    .pipe( plumber( {
+      errorHandler: notify.onError( {
+        title: "Ошибка в CSS",
+        message: "Error: <%= error.message %>"
+      } )
+    } ) )
+    .pipe( sourcemaps.init() )
+    .pipe( scss() )
+    .pipe( criticalCss() )
+    .pipe( autoprefixer({
+      cascade: false
+    } ) )
+    .pipe( csscomb() )
+    .pipe( gulp.dest( path.build.css ) )
+    .pipe( csso({
+      comments: false
+    } ) )
+    .pipe( rename({
+      suffix: '.min'
+    } ) )
+    .pipe( sourcemaps.write() )
+    .pipe( gulp.dest( path.build.css ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.css)
-    .pipe(plumber())
-    .pipe(scss())
-    .pipe(autoprefixer({ cascade: false }))
-    .pipe(csscomb())
-    .pipe(gulp.dest(path.build.css))
-    .pipe(csso({ comments: false }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(path.build.css))
-    .pipe(browserSync.stream());
 }
 
 
 // JS ============================
 
-function scripts() {
+function compileJS() {
 
-  console.log('Компиляция JS...');
+  return gulp.src( path.src.js )
+    .pipe( plumber( {
+      errorHandler: notify.onError( {
+        title: "Ошибка в JS",
+        message: "Error: <%= error.message %>"
+      } )
+    } ) )
+    .pipe( sourcemaps.init() )
+    .pipe( gulp.dest( path.build.js ) )
+    .pipe( uglify() )
+    .pipe( rename({
+      suffix: '.min'
+    }) )
+    .pipe( sourcemaps.write() )
+    .pipe( gulp.dest( path.build.js ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.js)
-    .pipe(plumber())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(path.build.js))
-    .pipe(browserSync.stream());
+}
+
+function assemblyVendor() {
+
+  return gulp.src( path.src.ven )
+    .pipe(newer( path.build.ven ) )
+    .pipe(gulp.dest( path.build.ven ) )
+    .pipe( browserSync.stream() );
+
 }
 
 
@@ -142,22 +184,22 @@ function scripts() {
 
 function convertTTFtoWOFF() {
 
-  console.log('Конвертация ttf в woff...');
+  return gulp.src( path.src.font )
+    .pipe(newer( path.build.font ) )
+    .pipe( ttf2woff() )
+    .pipe( gulp.dest(path.build.font) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.font)
-    .pipe(ttf2woff())
-    .pipe(gulp.dest(path.build.font))
-    .pipe(browserSync.stream());
 }
 
 function convertTTFtoWOFF2() {
 
-  console.log('Конвертация ttf в woff2...');
+  return gulp.src( path.src.font )
+    .pipe( newer( path.build.font ) )
+    .pipe( ttf2woff2() )
+    .pipe( gulp.dest( path.build.font ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.font)
-    .pipe(ttf2woff2())
-    .pipe(gulp.dest(path.build.font))
-    .pipe(browserSync.stream());
 }
 
 
@@ -165,56 +207,73 @@ function convertTTFtoWOFF2() {
 
 function optimizeImages() {
 
-  console.log('Оптимизация изображений...');
-
-  return gulp.src(path.src.img + '**/*.{jpg,png,svg}')
-    .pipe(imagemin([
+  return gulp.src( path.src.img + '**/*.{jpg,png,svg}' )
+    .pipe( newer( path.build.img ) )
+    .pipe( imagemin([
       imagemin.optipng(),
-      imagemin.svgo({
+      imagemin.svgo( {
         plugins: [
           {removeViewBox: false},
           {removeTitle: true},
           {cleanupNumericValues:
-            {floatPrecision: 0}
+              {floatPrecision: 0}
           }
         ]
-      }),
+      } ),
       imageminMozjpeg({
         quality: 80
-      })
-    ]))
-    .pipe(gulp.dest(path.build.img))
-    .pipe(browserSync.stream());
-}
+      } )
+    ] ) )
+    .pipe( gulp.dest( path.build.img) )
+    .pipe( browserSync.stream() );
 
+}
 
 function convertImagesToWebp() {
 
-  console.log('Конвертирование изображений в WebP...');
+  return gulp.src( path.src.img + '**/*.{jpg,png}' )
+    .pipe( newer( path.build.img ) )
+    .pipe( imagemin([
+      webp({
+        quality: 80
+      } )
+    ] ) )
+    .pipe( rename({
+      extname: '.webp'
+    } ) )
+    .pipe( gulp.dest( path.build.img ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.img + '**/*.{jpg,png}')
-    .pipe(imagemin([
-      webp({ quality: 80 })
-    ]))
-    .pipe(rename({ extname: '.webp' }))
-    .pipe(gulp.dest(path.build.img))
-    .pipe(browserSync.stream());
 }
 
 
 // ICO ============================
 
-function collectIconsToSvgSprite() {
+function assemblySvgSprites() {
 
-  console.log('Сборка спрайта SVG...');
+  return gulp.src( path.src.ico )
+    .pipe( svgMinify() )
+    .pipe( gulp.dest( path.build.ico ) )
+    .pipe( svgSprite({
+      mode: {
+        css: {
+          dest: '.',
+          bust: false,
+          sprite: '../icons/sprite.svg',
+          layout: 'vertical',
+          prefix: '%-svg-',
+          dimensions: true,
+          render: {
+            scss: {
+              dest: '_sprite.scss'
+            }
+          }
+        }
+      }
+    }))
+    .pipe( gulpIf( '*.scss', gulp.dest( root.src + 'styles/helpers'), gulp.dest( path.build.ico ) ) )
+    .pipe( browserSync.stream() );
 
-  return gulp.src(path.src.ico)
-    .pipe(svgMinify())
-    .pipe(gulp.dest(path.build.ico))
-		.pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename('symbols.svg'))
-    .pipe(gulp.dest(path.build.ico))
-    .pipe(browserSync.stream());
 }
 
 
@@ -222,44 +281,67 @@ function collectIconsToSvgSprite() {
 
 function optimizeVideos() {
 
-  console.log('Переброс MP4...');
+  return gulp.src( path.src.vid )
+    .pipe( newer( path.build.vid ) )
+    .pipe( gulp.dest( path.build.vid ) )
+    .pipe( browserSync.stream());
 
-  return gulp.src(path.src.vid)
-    .pipe(gulp.dest(path.build.vid))
-    .pipe(browserSync.stream());
-}
-
-
-// SERVER ============================
-
-function server() {
-  browserSync.init({ 
-    server: path.build.html,
-    cors: true,
-    notify: false 
-  });
-
-  gulp.watch(path.watch.php, compilePHP);
-  gulp.watch(path.watch.html, compileMarkup);
-  gulp.watch(path.watch.css, compileStyles);
-  gulp.watch(path.watch.js, scripts);
-  gulp.watch(path.watch.font, convertTTFtoWOFF);
-  gulp.watch(path.watch.font, convertTTFtoWOFF2);
-  gulp.watch(path.watch.img + '**/*.{jpg,png,svg}', optimizeImages);
-  gulp.watch(path.watch.img + '**/*.{jpg,png}', convertImagesToWebp);
-  gulp.watch(path.watch.ico, collectIconsToSvgSprite);
-  gulp.watch(path.watch.vid, optimizeVideos);
-
-  gulp.watch(path.watch.html).on('change', browserSync.reload);
 }
 
 
 // BUILD ============================
 
-var build = gulp.series(clean, gulp.parallel(compilePHP, compileMarkup, compileStyles, scripts, convertTTFtoWOFF, convertTTFtoWOFF2, optimizeImages, convertImagesToWebp, collectIconsToSvgSprite, optimizeVideos));
+exports.cleanDirectory = cleanDirectory;
+
+const build = gulp.series(cleanDirectory, gulp.parallel(
+  compilePHP,
+  compileHTML,
+  compileCSS,
+  compileJS,
+  assemblyVendor,
+  convertTTFtoWOFF,
+  convertTTFtoWOFF2,
+  optimizeImages,
+  convertImagesToWebp,
+  assemblySvgSprites,
+  optimizeVideos
+));
+
 exports.build = build;
 
 
-// DEFAULT ============================
+// SERVER ============================
 
-exports.default = gulp.series(build, server);
+function runServer() {
+
+  browserSync.init( {
+    server: root.build,
+    cors: true,
+    notify: false
+  } );
+
+  browserSync.watch( root.src + '**/*.*' ).on( 'change', browserSync.reload );
+
+}
+
+
+// WATCH ============================
+
+function watchFiles() {
+  gulp.watch( path.watch.php, compilePHP );
+  gulp.watch( path.watch.html, compileHTML );
+  gulp.watch( path.watch.css, compileCSS );
+  gulp.watch( path.watch.js, compileJS );
+  gulp.watch( path.watch.ven, assemblyVendor );
+  gulp.watch( path.watch.font, convertTTFtoWOFF );
+  gulp.watch( path.watch.font, convertTTFtoWOFF2 );
+  gulp.watch( path.watch.img + '**/*.{jpg,png,svg}', optimizeImages );
+  gulp.watch( path.watch.img + '**/*.{jpg,png}', convertImagesToWebp );
+  gulp.watch( path.watch.ico, assemblySvgSprites );
+  gulp.watch( path.watch.vid, optimizeVideos );
+}
+
+
+// DEVELOPMENT ============================
+
+exports.development = gulp.series( build, gulp.parallel( runServer, watchFiles ) );
